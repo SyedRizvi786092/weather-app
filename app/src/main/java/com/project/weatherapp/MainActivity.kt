ad9a2090ee033a7b9a189a49c0dbaa47b81df52d
Mohd Mojiz Hasan <mohdmojiz.stdnt@srmu.ac.in>
2024-06-29 03:49:15 +0530

    Final touches v1

--- app/src/main/java/com/project/weatherapp/MainActivity.kt
+++ app/src/main/java/com/project/weatherapp/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         weatherTask().execute()
     }
 
-    inner class weatherTask() : AsyncTask<String, Void, Pair<String?,String?>>() {
+    inner class weatherTask() : AsyncTask<String, Void, String?>() {
         override fun onPreExecute() {
             super.onPreExecute()
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
@@ -33,57 +33,33 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.error).visibility = View.GONE
         }
 
-        fun callAPI(url: String): String? {
+        override fun doInBackground(vararg p0: String?): String? {
             var response: String?
             try {
-                response = URL(url)
-                    .readText(Charsets.UTF_8)
+                response = URL("https://api.weatherapi.com/v1/forecast.json?q=$CITY&days=1&key=$API").readText(Charsets.UTF_8)
             } catch (e: Exception) {
                 response = null
             }
             return response
         }
 
-        override fun doInBackground(vararg p0: String?): Pair<String?, String?> {
-            var currentApiResponse: String?
-            var astroApiResponse: String?
-            var forecastApiResponse: String?
-            try {
-                currentApiResponse = callAPI("https://api.weatherapi.com/v1/current.json?q=$CITY&key=$API")
-                val currentDate = LocalDate.now().toString()
-                astroApiResponse = callAPI("https://api.weatherapi.com/v1/astronomy.json?q=$CITY&dt=$currentDate&key=$API")
-                forecastApiResponse = callAPI("https://api.weatherapi.com/v1/forecast.json?q=$CITY&days=1&key=$API")
-            } catch (e: Exception) {
-                currentApiResponse = null
-                astroApiResponse = null
-                forecastApiResponse = null
-            }
-            return Pair(currentApiResponse, forecastApiResponse)
-        }
-
-        override fun onPostExecute(result: Pair<String?,String?>) {
+        override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             try {
-                val jsonObj1 = JSONObject(result.first.toString())
-                val location = jsonObj1.getJSONObject("location")
-                val current = jsonObj1.getJSONObject("current")
+                val jsonObj = JSONObject(result.toString())
+
+                val location = jsonObj.getJSONObject("location")
+                val current = jsonObj.getJSONObject("current")
                 val updatedAt: Long = current.getLong("last_updated_epoch")
-                val updatedAtText =
-                    "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
-                        Date(updatedAt * 1000)
-                    )
+                val updatedAtText = "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt * 1000))
                 val temp = current.getDouble("temp_c").toString() + "°C"
                 val pressure = current.getDouble("pressure_mb").toString() + " mb"
                 val humidity = current.getDouble("humidity").toString() + "%"
                 val windSpeed = current.getDouble("wind_kph").toString() + " km/h"
                 val status = current.getJSONObject("condition").getString("text")
-                val address =
-                    location.getString("name") + ", " + location.getString("region") + ", " + location.getString(
-                        "country"
-                    )
+                val address = location.getString("name") + ", " + location.getString("region") + ", " + location.getString("country")
 
-                val jsonObj2 = JSONObject(result.second.toString())
-                val forecast = jsonObj2.getJSONObject("forecast").getJSONArray("forecastday").getJSONObject(0)
+                val forecast = jsonObj.getJSONObject("forecast").getJSONArray("forecastday").getJSONObject(0)
                 val astro = forecast.getJSONObject("astro")
                 val day = forecast.getJSONObject("day")
                 val sunrise = astro.getString("sunrise")
